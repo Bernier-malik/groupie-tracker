0e50e0fe13958cb19e6c0f06d95ba06b512272f8
byacine <pythag0re@ymachine>
2025-03-25 09:40:46 +0100

    ajout des routes pour les jeux et l'inscription

--- server/server.go
+++ server/server.go
@@ -5,11 +5,37 @@ import (
 	"net/http"
 )
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "PAGE ACCEUIL")
+}
+func guessSoundHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Guess The Sound")
+}
+func petitBacHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Petit Bac")
+}
+func BlindTestHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Blind test")
+}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Page de connexion")
+}
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Inscrivez vous")
+}
+
 func Start() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "")
 	})
 
-	fmt.Println("Serveur démarré sur le port 8080...")
+	http.HandleFunc("/home", homeHandler)
+	http.HandleFunc("/guess", guessSoundHandler)
+	http.HandleFunc("/petit", petitBacHandler)
+	http.HandleFunc("/blind", BlindTestHandler)
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/register", registerHandler)
+
+	fmt.Println("Serveur démarré sur le port 8080 ")
 	http.ListenAndServe(":8080", nil)
 }
