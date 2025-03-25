d67f5dd1afdff50f5ebab26c0326ea06fad6b476
Bernier-malik <malik@pc-malik>
2025-03-25 09:35:27 +0100

    clé API

---
+++ API.go
@@ -0,0 +1,6 @@
+package main
+
+//curl -X POST "https://accounts.spotify.com/api/token"      -H "Content-Type: application/x-www-form-urlencoded"      -d "grant_type=client_credentials&client_id=454055576c254e1e802bf0b10f687298&client_secret=3fe51de5734a4052bceac38e4cb35a42"
+
+var API string = "BQBLF247gcT2ShUVjk6SVzI3VUA7RmTmn8mBhl0hFuO3caT9hzdtcDiB-VFTsfHOfQ0bzAPQzkTM_0W5xu0EV9w0vqe_AYnHgGiKzxZatrl9JqYJIcM7tC3c8OgquluBU2tPqSX_qas"
+println(API)
