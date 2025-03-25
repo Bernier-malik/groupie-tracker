8a198952af66a3baeab6cf9f4bebf37acf4a0a63
Bernier-malik <malik.bernier@ynov.com>
2025-03-25 11:19:02 +0100

    recuperation de la playliste

--- API.go
+++ API.go
@@ -1,6 +1,34 @@
-package main
 
 //curl -X POST "https://accounts.spotify.com/api/token"      -H "Content-Type: application/x-www-form-urlencoded"      -d "grant_type=client_credentials&client_id=454055576c254e1e802bf0b10f687298&client_secret=3fe51de5734a4052bceac38e4cb35a42"
+package main
 
-var API string = "BQBLF247gcT2ShUVjk6SVzI3VUA7RmTmn8mBhl0hFuO3caT9hzdtcDiB-VFTsfHOfQ0bzAPQzkTM_0W5xu0EV9w0vqe_AYnHgGiKzxZatrl9JqYJIcM7tC3c8OgquluBU2tPqSX_qas"
-println(API)
+import (
+	"context"
+	"log"
+
+	"github.com/zmb3/spotify"
+	"golang.org/x/oauth2/clientcredentials"
+)
+
+func main() {
+	authConfig := &clientcredentials.Config{
+		ClientID:     "454055576c254e1e802bf0b10f687298",
+		ClientSecret: "3fe51de5734a4052bceac38e4cb35a42",
+		TokenURL:     spotify.TokenURL,
+	}
+
+	accessToken, err := authConfig.Token(context.Background())
+	if err != nil {
+		log.Fatalf("error retrieve access token: %v", err)
+	}
+
+	client := spotify.Authenticator{}.NewClient(accessToken)
+
+	playlistID := spotify.ID("0s3c0flbAQIvlvG5v1d7X8")
+	playlist, err := client.GetPlaylist(playlistID)
+	if err != nil {
+		log.Fatalf("error retrieve playlist data: %v", err)
+	}
+	
+	log.Println("playlist name:", playlist.Name)
+}
