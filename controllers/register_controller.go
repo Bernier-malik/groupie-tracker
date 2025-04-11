7097123de9d2164f5441917e5155656b7d1ce59c
byacine <pythag0re@ymachine>
2025-04-11 14:56:22 +0200

    add hash

--- controllers/register_controller.go
+++ controllers/register_controller.go
@@ -6,7 +6,14 @@ import (
 	"groupie/db"
 	"log"
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
 
 // RegisterUser handles user registration
 func RegisterUser(w http.ResponseWriter, r *http.Request) {
@@ -20,6 +27,7 @@ func RegisterUser(w http.ResponseWriter, r *http.Request) {
 	pseudo := r.FormValue("pseudo")
 	password := r.FormValue("password")
 	confirmPassword := r.FormValue("confirm-password")
+	HasehPassword, _ := HashPassword(password)
 
 	// Step 1: Check if passwords match
 	if password != confirmPassword {
@@ -48,7 +56,7 @@ func RegisterUser(w http.ResponseWriter, r *http.Request) {
 
 	// Step 3: Insert new user into the database
 	queryInsert := `INSERT INTO players (email, pseudo, password) VALUES (?, ?, ?)`
-	_, err = db.DB.Exec(queryInsert, email, pseudo, password)
+	_, err = db.DB.Exec(queryInsert, email, pseudo, HasehPassword)
 	if err != nil {
 		log.Println("Error inserting user:", err)
 		http.Error(w, "Error creating user", http.StatusInternalServerError)
--- go.mod
+++ go.mod
@@ -1,4 +1,8 @@
 module groupie
 
 go 1.23.0
-require github.com/mattn/go-sqlite3 v1.14.24 // indirect
+
+require (
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+	golang.org/x/crypto v0.37.0
+)
